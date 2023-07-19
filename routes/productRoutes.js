99d2cd40aa8924a4aeed7e24cb8767b5590273ff
Fru Boris <fruboris24@gmail.com>
2023-07-19 11:36:57 +0100

    fix: product route encapsulation

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,80 +1,35 @@
 const express = require("express");
 const router = express.Router();
-const Product = require("../models/product");
-
+const {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productController");
 // Get all products
 router.get("/", async (req, res) => {
-  try {
-    const products = await Product.findAll();
-    res.json(products);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error" });
-  }
+  await getAllProducts(req, res);
 });
 
 // Get a specific product
 router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const product = await Product.findByPk(id);
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-    res.json(product);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error" });
-  }
+  await getProductById(req, res);
 });
 
 // Create a new product
 router.post("/", async (req, res) => {
-  const { name, size, category, price } = req.body;
-  try {
-    const product = await Product.create({ name, size, category, price });
-    res.status(201).json(product);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error" });
-  }
+  await createProduct(req, res);
 });
 
 // Update a product
 router.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const { name, size, category, price } = req.body;
-  try {
-    const product = await Product.findByPk(id);
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-    product.name = name;
-    product.size = size;
-    product.category = category;
-    product.price = price;
-    await product.save();
-    res.json(product);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error" });
-  }
+  await updateProduct(req, res);
 });
 
 // Delete a product
 router.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const product = await Product.findByPk(id);
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-    await product.destroy();
-    res.json({ message: "Product deleted successfully" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error" });
-  }
+  await deleteProduct(req, res);
 });
 
 module.exports = router;
