f10233f9121fcd7fb0bebea937af55df3f1bd302
Fru Boris <fruboris24@gmail.com>
2023-07-19 13:51:36 +0100

    feat: add cart attribute to the user table

--- migrations/add-column-product-table.js
+++ migrations/add-column-product-table.js
@@ -27,16 +27,6 @@ module.exports = {
         type: Sequelize.FLOAT,
         allowNull: false,
       },
-      createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
     });
   },
   down: async (queryInterface, Sequelize) => {
