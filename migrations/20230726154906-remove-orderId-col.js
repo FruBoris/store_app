b24ebd9c1e417530b495eb2eef94beee82fc5cad
Fru Boris <fruboris24@gmail.com>
2023-07-26 16:53:22 +0100

    feat: migration file to delete

---
+++ migrations/20230726154906-remove-orderId-col.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     */
+    await queryInterface.removeColumn("Payments", "orderId");
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
